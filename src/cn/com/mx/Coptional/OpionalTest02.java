e177a7bb5de4dc1517bb17ae2343d80efbaefd36
wangchangye <wangchangye@gomeplus.com>
2016-10-24 13:23:48 +0800

    修改

--- src/cn/com/mx/Coptional/OpionalTest02.java
+++ src/cn/com/mx/Coptional/OpionalTest02.java
@@ -11,8 +11,13 @@ import java.util.List;
 public class OpionalTest02 {
 	static List<Student> list = StudentFactory.createStudent();
 	public static void main(String[] args) {
-		//test01();
-		//test02();
+		System.err.println("-------------1-----------");
+		test01();
+		System.err.println("-------------2-----------");
+		test02();
+		System.err.println("-------------3-----------");
+		test03();
+		System.err.println("-------------4-----------");
 		test04();
 	}
 	
@@ -29,11 +34,11 @@ public class OpionalTest02 {
 		return Arrays.stream(stu.getScores()).average().orElse(0);
 	}
 	private static void test03(){
-		list.forEach((u)->System.out.println(u+"=="+ave(u)));
+		list.stream().forEach((u)->System.out.println(u+"=="+ave(u)));
 	}
 	private static void test04(){
-		list.stream().filter(x->x.getId()==2)
-					.mapToInt(x->(Arrays.stream(x.getScores()).min()).orElse(0))
+		list.stream().filter(x->x.getId()==1)
+					.mapToInt(x->(Arrays.stream(x.getScores()).max()).orElse(0))
 					.forEach(System.out::println);
 	}
 
