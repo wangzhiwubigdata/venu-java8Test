ee20ae653241926d27ad50ac47f9da2b548d5966
wangchangye <wangchangye@gomeplus.com>
2017-02-13 18:47:04 +0800

    添加一些测试

--- src/cn/com/mx/lambda/TestC.java
+++ src/cn/com/mx/lambda/TestC.java
@@ -1,14 +1,12 @@
 package cn.com.mx.lambda;
 
-import java.util.ArrayList;
 import java.util.List;
+import java.util.stream.Collectors;
+import java.util.stream.IntStream;
 
 public class TestC {
 	public static void main(String[] args) {
-		List<User> users = new ArrayList<User>();
-		for (int i = 0; i < 10; i++) {
-			users.add(new User(i,"billy"+Integer.toString(i)));
-		}
+		List<User> users = IntStream.range(0, 10).mapToObj(i -> new User(i, "billy" + Integer.toString(i))).collect(Collectors.toList());
 		users.stream().map(User::getName).forEach(System.out::println);
 	}
 }
